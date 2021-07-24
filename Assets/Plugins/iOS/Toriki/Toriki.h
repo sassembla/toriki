10bdb4d71cce890b22dd758ceec0e777a8f69364
sassembla <toru.inoue@kissaki.tv>
2021-07-24 13:08:55 +0900

    fixed: build iOS project on Unity 2020.3.x

--- Assets/Plugins/iOS/Toriki/Toriki.h
+++ Assets/Plugins/iOS/Toriki/Toriki.h
@@ -1,6 +1,5 @@
 #import <Foundation/Foundation.h>
 #import "AppDelegateListener.h"
-#include "RegisterMonoModules.h"
 
 @interface Toriki : NSObject <AppDelegateListener>
 + (Toriki *)sharedInstance;
